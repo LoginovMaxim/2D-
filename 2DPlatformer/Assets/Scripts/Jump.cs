56af01418dbf8a4d2ffc37e0026b542f2da1b5c0
LoginovMaxim <60643643+LoginovMaxim@users.noreply.github.com>
2020-03-27 19:45:56 +0700

    Unity 2D Platformer
    создал проект

---
+++ 2DPlatformer/Assets/Scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
